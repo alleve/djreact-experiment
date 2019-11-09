d112a50ebbbc5bd093934df8ce478cb78919557d
Allen S. Rout <asr@routhouse.org>
2019-11-09 17:37:26 -0500

    More tutorial.

--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'leads',  # Following the tutorial.
+    'leads',
+    'frontend',
 
 ]
 
