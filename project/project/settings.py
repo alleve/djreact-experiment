8dc9d7e621d65b19221bf5caba2d1ef61e0393a9
Allen S. Rout <asr@routhouse.org>
2019-11-09 13:19:45 -0500

    Erroneous cut.

--- project/project/settings.py
+++ project/project/settings.py
@@ -37,7 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'leads',  # Following the tutorial.FrameOptionsMiddleware',
+    'leads',  # Following the tutorial.
+
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'project.urls'
